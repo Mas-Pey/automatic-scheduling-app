304dc201a2a49b320d26ba6e29d9eef5ddd51834
Mas-Pey <konamexxxpes@gmail.com>
2025-11-07 21:32:14 +0700

    refactor: move Employee Component to child

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,15 @@
 import { useState } from 'react';
 import '@zach.codes/react-calendar/dist/calendar-tailwind-no-reset.css';
-import { useEmployees } from './hooks/useEmployees';
 import { useSchedule } from './hooks/useSchedule';
-import { EmployeeList } from './components/EmployeeList';
 import { ScheduleCalendar } from './components/ScheduleCalendar';
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState<Date>(new Date())
-  const {
-    employees,
-    showList,
-    fetchEmployees,
-    setShowList,
-    updateEmployee,
-    addEmployee,
-    deleteEmployee
-  } = useEmployees()
+  
   const { schedules, summary, createSchedule } = useSchedule()
 
   return (
-    <div className='mx-auto max-w-5xl py-4'>
-      <button
-        onClick={fetchEmployees}
-        className='p-2 bg-amber-600 text-white text-md rounded-md hover:bg-amber-700'
-      >
-        Load Employees
-      </button>
-
-      <EmployeeList
-        employees={employees}
-        show={showList}
-        onHide={() => setShowList(false)}
-        onUpdate={updateEmployee}
-        onAdd={addEmployee}
-        onDelete={deleteEmployee}
-      />
-
+    <div className='mx-auto max-w-5xl py-5'>
       <ScheduleCalendar
         currentMonth={currentMonth}
         setCurrentMonth={setCurrentMonth}
